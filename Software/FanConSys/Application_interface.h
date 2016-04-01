b18192e78cbe008f43885fcd242a38c192940f08
CRIZMA-PC&LAPTOP <CRIZMA-PC&LAPTOP@192.168.1.106>
2016-04-01 19:10:35 +0200

    Add The Application Layer Add the main file

---
+++ Software/FanConSys/Application_interface.h
@@ -0,0 +1,18 @@
+/*
+ * Application_interface.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: CRIZMA-PC&LAPTOP
+ */
+
+#ifndef APPLICATION_INTERFACE_H_
+#define APPLICATION_INTERFACE_H_
+
+
+
+extern void Application_voidInit(void);
+
+
+extern void FanControlSys(void);
+
+#endif /* APPLICATION_INTERFACE_H_ */
